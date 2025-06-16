9337dce290bacc72ab428b71b73a9a68ddd933eb
NahlaAboromi <Nahla.Aboromi@e.braude.ac.il>
2025-06-16 22:18:05 +0300

    Update App.jsx NAHLA

--- hw2-frontend/src/App.jsx
+++ hw2-frontend/src/App.jsx
@@ -16,7 +16,7 @@ import ViewStudentSimulation from "./studentPages/StudentSimulation.jsx";
 import { UserProvider } from './context/UserContext';
 import { ThemeProvider } from './DarkLightMood/ThemeContext';
 import { NotificationsProvider } from "./context/NotificationsContext";
-import { StudentNotificationsProvider } from "./context/StudentNotificationsContext";
+import { StudentNotificationsProvider } from "./context/StudentNotificationContext";
 import StudentHome from './studentPages/StudentHome';
 import StudentRecentActivites from './studentPages/StudentRecentActivities';
 import MyReports from './studentPages/MyReports';
