82a2e7accfaf1798099889148408618fcad9b217
NahlaAboromi <Nahla.Aboromi@e.braude.ac.il>
2025-06-18 21:28:44 +0300

    Update RecentActivity.jsx

--- hw2-frontend/src/components/RecentActivity.jsx
+++ hw2-frontend/src/components/RecentActivity.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { UserContext } from './UserContext';
+import { UserContext } from '../context/UserContext';
 
 const NotificationsContext = createContext();
 
