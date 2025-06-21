aa20f8e60823c7cdfdc28814424dcacee7b300f8
Manal2308 <manal.nama@outlook.com>
2025-06-22 02:02:16 +0300

    Update StudentRecentActivities.jsx

--- hw2-frontend/src/studentPages/StudentRecentActivities.jsx
+++ hw2-frontend/src/studentPages/StudentRecentActivities.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect,useContext } from 'react';
-import { StudentNotificationsContext } from '../context/StudentNotificationsContext';
+import { useStudentNotification } from '../context/StudentNotificationsContext';
 import { useLocation } from 'react-router-dom';
 
 const RecentActivity = () => {
-  const { notifications, fetchNotifications } = useContext(StudentNotificationsContext);
+  const { notifications, fetchNotifications } = useStudentNotification();
   const location = useLocation();
 
   // Fetch notifications every time the pathname changes (page changes)
